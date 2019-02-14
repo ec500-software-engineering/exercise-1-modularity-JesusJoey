7ae3d5608b5fe8c4c09038899f20c72f94b18f5a
Joey <43118582+JesusJoey@users.noreply.github.com>
2019-02-13 20:43:23 -0500

    Implementation of a simple I/O system

--- EC500-Modular-design/Implement.py
+++ EC500-Modular-design/Implement.py
@@ -1,22 +1,15 @@
-import sys
-from Input_module import input_module
+import time
+import threading
+import  Input_module
 from Analyzer import Analyzer
 import Output_Module
+from queue import Queue
 
-
-def main():
+def process():
     # user_id, age, gender, heartrate, Systolic_BP, Diastolic_BP, blood_oxygen, temperature, time):
-    data = {}
-    LINES=open("input").read().splitlines()
-    for idx,line in enumerate(LINES):
-        items = line.split()
-        #print(items)
-        Data=input_module(items[0],items[1],items[2],items[3],items[4],items[5],items[6],items[7],items[8])
-        #Data=input_module.implement_filter(Data)
-        data[idx]=Data.dic
-        print data[idx]
-
     #def __init__(self, Systolic_BP, Diastolic_BP, Heart_Rate, Heart_Oxy_Level, Body_temp):
+    data=Input_module.input()
+    time.sleep(0.5)
     for idx,content in enumerate(data):
         process=Analyzer(data[idx]["Systolic_BP"],data[idx]["Diastolic_BP"],data[idx]["heartrate"],data[idx]["blood_oxygen"],data[idx]["temperature"])
         signal_loss=process.Signal_Loss(data[idx]["heartrate"],data[idx]["temperature"])
@@ -27,10 +20,21 @@ def main():
         hypertension=process.Hypertension(data[idx]["Systolic_BP"],data[idx]["Diastolic_BP"])
 
         result=Output_Module.display_basic_iuput_data(signal_loss, shock_alert, oxygen_supply, fever, hypotension, hypertension)
-        print 'Patient',idx,'Alert'
-        print result
+        print('--------------------------------------')
+        print 'Patient  No', idx, 'Alert'
+        for index in result:
+            print index,':',result[index]
 
 
+def main():
+    t1 = threading.Thread(target=Input_module.input())
+    t2 = threading.Thread(target=process)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
 
 
 
