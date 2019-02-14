704f28dbf2ddf7362d12cd307ba2b13834265185
Joey <43118582+JesusJoey@users.noreply.github.com>
2019-02-13 20:43:47 -0500

    Implementation of a simple I/O system

--- EC500-Modular-design/Input_module.py
+++ EC500-Modular-design/Input_module.py
@@ -1,3 +1,4 @@
+import time
 class input_module:
 
     def __init__(self, user_id, age, gender, heartrate, Systolic_BP, Diastolic_BP, blood_oxygen, temperature, time):
@@ -15,13 +16,13 @@ class input_module:
                     "Diastolic_BP": Diastolic_BP, "Systolic_BP":Systolic_BP, "blood_oxygen": blood_oxygen,
                     "temperature": temperature, "time": time}
 
-    def filter(data):
+    '''def filter(data):
         wrong_flag = -1
         noise = 500
         if data > noise:
             data = wrong_flag
         return data
-
+    '''
 
 
     def implement_filter(self):
@@ -49,3 +50,21 @@ class input_module:
 #my_data.implement_filter()
 #print(my_data.dic)
 #print(my_data.return_request())
+
+def input():
+    # user_id, age, gender, heartrate, Systolic_BP, Diastolic_BP, blood_oxygen, temperature, time):
+    data={}
+    LINES=open("input").read().splitlines()
+    time.sleep(0.5)
+    for idx,line in enumerate(LINES):
+        items = line.split()
+        #print(items)
+        Data=input_module(items[0],items[1],items[2],items[3],items[4],items[5],items[6],items[7],items[8])
+        #Data=input_module.implement_filter(Data)
+        data[idx]=Data.dic
+        #print data[idx]
+    return data
+
+data = input()
+for item in data:
+    print data[item]
